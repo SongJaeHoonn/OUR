9e187356f1c4fdcf4dcf27774fcf537271b282e6
SongJaeHoonn <thdwogns121@naver.com>
2024-01-02 13:47:14 +0900

    회원가입/로그인 컨트롤러 생성

--- OUR/src/main/java/app/Controller/MemberController.java
+++ OUR/src/main/java/app/Controller/MemberController.java
@@ -1,9 +1,41 @@
 package app.Controller;
 
+import app.Dto.MemberDto;
+import app.Service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
-    @GetMapping
+    private final MemberService memberService;
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute MemberDto memberDto){
+        memberService.save(memberDto);
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute MemberDto memberDto, HttpSession httpSession){
+        MemberDto loginresult = memberService.login(memberDto);
+        if(loginresult != null){
+            httpSession.setAttribute("loginAlias", loginresult.getAlias());
+            return "main";
+        }else{
+            return "login";
+        }
+    }
+
+
 }
