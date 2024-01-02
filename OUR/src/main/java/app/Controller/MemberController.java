5cbef0181a9766142d7f445dd8822ccd7cc24dbf
SongJaeHoonn <thdwogns121@naver.com>
2024-01-02 11:40:36 +0900

    홈 컨트롤러 생성

--- OUR/src/main/java/app/Controller/MemberController.java
+++ OUR/src/main/java/app/Controller/MemberController.java
@@ -1,4 +1,9 @@
 package app.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberController {
+    @GetMapping
 }
