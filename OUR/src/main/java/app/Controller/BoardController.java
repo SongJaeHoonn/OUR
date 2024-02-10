a380e86f8697223b6f090b1e7176b26b0aedeeb2
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-02-10 16:47:56 +0900

    글 작성 로직 완성(리스트가 메인에 계속 보여야 하는데 안보임)

--- OUR/src/main/java/app/Controller/BoardController.java
+++ OUR/src/main/java/app/Controller/BoardController.java
@@ -1,8 +1,6 @@
 package app.Controller;
-
 import app.Dto.BoardDto;
 import app.Service.BoardService;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.time.LocalDateTime;
+import java.sql.Date;
 import java.util.List;
 
 @Controller
@@ -21,40 +17,34 @@ import java.util.List;
 @RequestMapping("/our")
 public class BoardController {
 
-    BoardService boardService;
+    private final BoardService boardService;
 
     @GetMapping("/create")
-    public String createForm(){
+    public String createForm() {
         return "create";
     }
 
     @PostMapping("/main")
-    public String create(BoardDto boardDto, HttpSession session){
-        boardDto.setMember((String)session.getAttribute("loginMemberId"));
+    public String create(BoardDto boardDto) {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        // Timestamp를 java.sql.Date로 변환합니다.
         Date date = new Date(timestamp.getTime());
+        // BoardDto에 날짜 정보를 설정합니다.
         boardDto.setDate(date);
+
         boardService.createPost(boardDto);
-        return "main";
+
+        // 생성된 게시물의 상세 정보 페이지로 리디렉션합니다.
+        return "redirect:/our/main";
     }
 
     @GetMapping("/main")
-    public String boardList(Model model){
+    public String boardList(Model model) {
         List<BoardDto> boardDtoList = boardService.findAll();
-        model.addAttribute("boardList",boardDtoList);
+        model.addAttribute("boardList", boardDtoList);
         return "main";
     }
-
-
-
-
-
-
-
-
-
 }
-
 
 
 
